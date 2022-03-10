334f0426f59e05b63a8a50b299b0e8d98bd428cb
simri77 <simretmesfin97@gmail.com>
2022-03-10 11:32:06 -0800

    0x00-hello_world

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,12 @@
-#!/bin/bash
+#include <studio.h>
+/**
+ * main - Entry point
+ *
+ * return: always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
 
