aa5ab833efdfe3822a2cdfe42d444ae7adb05fe4
simri77 <simretmesfin97@gmail.com>
2022-03-10 11:06:42 -0800

    0x00-hello_world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
-#include <studio.h>
+#include <stdio.h>
 /**
  * main - Entry point
  *
- * Return: Always 0 (success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	char hello[] = "\"Programming is like building a multilingual puzzle";
-	
+
 	puts(hello);
 	return (0);
 }
