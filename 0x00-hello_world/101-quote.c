f4d2e80c0a9089865f84ba41d63bbee995a51604
simri77 <simretmesfin97@gmail.com>
2022-03-10 12:39:48 -0800

    0x00-hello_world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,7 +2,7 @@
 #include <string.h>
 #include <unistd.h>
 /**
- * main - Entry point
+ * main - main block
  * Return: 1
  */
 int main(void)
@@ -10,8 +10,9 @@ int main(void)
 	int s;
 
 	s = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	
+
 	write(2,
-			"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",s);
+	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	      s);
 	return (1);
 }
