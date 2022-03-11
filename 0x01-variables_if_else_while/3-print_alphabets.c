b7a3620f406c5b6c89e881420b367685f244d9fa
simri77 <simretmesfin97@gmail.com>
2022-03-11 11:43:07 -0800

    0x01 variable if else while

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdioi.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alph;
+	char cap;
+
+	for (alph = 'a'; alph <= 'z'; alph++)
+	{
+		putchar (alph);
+	}
+	for (cap = 'A'; cap <= 'Z'; cap++)
+	{
+		putchar (cap);
+	}
+	putchar ('\n');
+	return (0);
+}
