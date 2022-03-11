d745ea559be666cc6617036cba54f71f77497840
simri77 <simretmesfin97@gmail.com>
2022-03-11 11:55:21 -0800

    0x01 variable if else while

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,9 +8,9 @@ int main(void)
 {
 	char alph;
 
-	for (alph = 'a'; alph <= 'z'; alph++)
+	for (alph = 'a';  alph <= 'z'; alph++)
 	{
-		if ((alph != 'q') && (alph !='e'))
+		if ((alph != 'q') && (alph != 'e'))
 		{
 			putchar (alph);
 		}
