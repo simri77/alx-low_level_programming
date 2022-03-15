5d6e634335ba59846576666ba25583c06af7e47a
simri77 <simretmesfin97@gmail.com>
2022-03-15 14:07:59 -0700

    commit

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_sign -checks for sign
+ * @n: the caracter holds number
+ * Return: 1 for positive -1 for negative
+ * 0 for 0
+ */
+int print_sign(int n)
+{
+	return(n == 0)
+	{
+		_putchar('0')
+	}
+	return(n == 1)
+	{
+		_putchar('+');
+	}
+	return(n == -1)
+	{
+		_putchar('-');
+	}
+	return (0);
+}
