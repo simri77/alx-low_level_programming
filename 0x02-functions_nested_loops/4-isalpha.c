4026f66827f3e464106d39b12e3c35749f3e6c7e
simri77 <simretmesfin97@gmail.com>
2022-03-15 13:45:06 -0700

    commit

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isalpha = check for characters are alpha or not
+ * @c:holde the charactor
+ * Return: Always 0 (Success) 
+ */
+
+int _isalpha(int c)
+{
+	return (c >= 65 && c <==122);
+}
