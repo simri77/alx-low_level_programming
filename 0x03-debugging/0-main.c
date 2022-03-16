820a0535b02ad331042321e0349309c3afac723d
simri77 <simretmesfin97@gmail.com>
2022-03-16 10:30:46 -0700

    0x03. C - Debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * positive_or_negative - check discription
+ *
+ * Discription: check if the int is positive_or_negative
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int positive_or_negative(void)
+	{
+	
+	}
+	return (0);
+}      
