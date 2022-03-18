4bff1d08255d3370aad8a87c2f74777e522e7819
simri77 <simretmesfin97@gmail.com>
2022-03-18 15:04:50 -0700

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - to multiply two numbers
+ * @a: it hold varable a
+ * @b: it hold the varabl b
+ * Return: Always 0 (Success)
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
