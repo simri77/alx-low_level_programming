8ec7eb34c8057fc7715c40ec2ee8c8d579fd9994
simri77 <simretmesfin97@gmail.com>
2022-03-25 12:35:22 -0700

    3 strncmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcmp - compare two strings
+ * @s1: first string
+ * @s2: second string
+ * Return: Always 0 (Success)
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] == s2[i]; i++)
+	{
+		if (s1[i] == '\0')
+		{
+			return (0);
+		}
+	}
+	return (s1[i] - s2[i]);
+}
