ec012e9c4967bc9ad239621005a14dd3f3cef9da
simri77 <simretmesfin97@gmail.com>
2022-03-28 12:24:13 -0700

    1 memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *_memcpy - copy funcion
+ *@dest: 
+ *@src:
+ *@n:
+ *Return: to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int x;
+
+	for (x = 0; x < n; x++)
+	{
+		dest[x] = src[x];
+	}
+
+	return (dest);
+}
