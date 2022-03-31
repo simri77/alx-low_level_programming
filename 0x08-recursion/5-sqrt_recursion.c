a8c3ffcf61e09f659836fb301cfd77c9e30fffc7
simri77 <simretmesfin97@gmail.com>
2022-03-31 13:04:12 -0700

    .c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -11,11 +11,11 @@
 int sqrt_recursion(int n, int i)
 
 {
-        if (i * i > n)
-                return (-1);
-        if (i * i == n)
-                return (i);
-        return (sqrt_recursion(n, i + 1));
+	if (i * i > n)
+		return (-1);
+	if (i * i == n)
+		return (i);
+	return (sqrt_recursion(n, i + 1));
 }
 
 /**
