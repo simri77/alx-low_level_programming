0df2640079ed0aaa4817400de203ac6b7d9bf06a
simri77 <simretmesfin97@gmail.com>
2022-03-31 12:54:02 -0700

    6 is prime.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,22 +1,6 @@
 #include "main.h"
 
 /**
- * is_prime_number - determine if a number is a prime number
- * @n: int number
- * Return: 1 if prime, 0 otherwise
- */
-
-int is_prime_number(int n)
-{
-	if (n < 2)
-		return (0);
-	if (n < 4)
-		return (1);
-
-	return (n, 2);
-}
-
-/**
  * _sqrt - return square root of number
  * @x: number
  * @i: number incrementer acting as divisor
@@ -41,12 +25,28 @@ int _sqrt(int x, int i)
  * Return: 0 if not prime, 1 if prime
  */
 
-int hai(int n, int d)
+int by(int n, int d)
 {
 	if (n % d == 0)
 		return (0);
 	else if (_sqrt(n, 1) < d)
 		return (1);
 	else
-		return (hai(n, d + 1));
+		return (by(n, d + 1));
+}
+
+/**
+ * is_prime_number - determine if a number is a prime number
+ * @n: int number
+ * Return: 1 if prime, 0 otherwise
+ */
+
+int is_prime_number(int n)
+{
+	if (n < 2)
+		return (0);
+	if (n < 4)
+		return (1);
+
+	return (by(n, 2));
 }
