c1863d981f4520510da3bd169d8dd1230e3c3966
simri77 <simretmesfin97@gmail.com>
2022-04-01 14:40:48 -0700

    2 args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entery point
+ * @argc: count of the argument supplied to the program
+ * @argv: array of pointer to the string declearation of the array
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char **argv)
+{
+	int i;
+	
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
