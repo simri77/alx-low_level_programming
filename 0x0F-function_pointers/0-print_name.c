0f6faad6672cc5b0eaa213edb94675a3bfdf3f84
simri77 <simretmesfin97@gmail.com>
2022-04-13 13:02:39 -0700

    0rint name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ *print_name - the pointer function
+ *
+ *@name: the name
+ *
+ *@f: the pointer
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
